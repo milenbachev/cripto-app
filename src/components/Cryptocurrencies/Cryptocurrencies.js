06c9afa3324310ec90bac09394788d4b30944db5
milenbachev <63249976+milenbachev@users.noreply.github.com>
2022-01-15 22:11:11 +0200

    implement cryptoDetails page

--- src/components/Cryptocurrencies/Cryptocurrencies.js
+++ src/components/Cryptocurrencies/Cryptocurrencies.js
@@ -37,10 +37,10 @@ const Cryptocurrencies = ({
             <Row gutter={[32, 32]} className='crypto-card-container'>
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
-                        <Link key={currency.uuid} to={`/crypto/${currency.rank}`}>
+                        <Link to={`/crypto/${currency.uuid}`}>
                             <Card
                                 title={`${currency.rank}. ${currency.name}`}
-                                extra={<img className='crypto-image' src={currency.iconUrl} alt='img'/>}
+                                extra={<img className='crypto-image' src={currency.iconUrl} alt=''/>}
                                 hoverable
                             >
                                 <p>Price: {millify(currency.price)}</p>
